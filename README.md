a794b182c60a7e194cff02f98fbd0c50586e6346
Paul R. Tagliamonte <paultag@gmail.com>
2021-03-14 15:18:54 -0400

    Add in a slightly better description

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 [![Go Reference](https://pkg.go.dev/badge/pault.ag/go/diskring.svg)](https://pkg.go.dev/pault.ag/go/diskring)
 [![Go Report Card](https://goreportcard.com/badge/pault.ag/go/diskring)](https://goreportcard.com/report/pault.ag/go/diskring)
+
+pault.ag/go/diskring contains an implementation of a ring buffer, backed by a
+file on the filesystem. This allows concurrent reads and writes by goroutines,
+overwriting data as needed, oldest-first.
+
+This buffer does not have fixed sizes, rather, it enocdes the length with
+the written data. Data is added and removed at the chunk level.
---
+++ doc.go
@@ -0,0 +1,29 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paultag@gmail.com> 2020-2021
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+// Package diskring contains an implementation of a ring buffer, backed by
+// a file on the filesystem. This allows concurrent reads and writes by
+// goroutines, overwriting data as needed, oldest-first.
+//
+// This buffer does not have fixed sizes, rather, it enocdes the length with
+// the written data. Data is added and removed at the chunk level.
+package diskring
+
+// vim: foldmethod=marker
